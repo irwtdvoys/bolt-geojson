3abae62b0497d807efc549aabdd4bc3808fe1cb3
Steve Adams <steve@3sidedcube.com>
2015-09-08 16:19:11 +0100

    Add Feature and FeatureCollection Classes

---
+++ src/Feature.php
@@ -0,0 +1,72 @@
+<?php
+	namespace Bolt\GeoJson;
+
+	use \Bolt\Base;
+
+	class Feature extends Base
+	{
+		public $type;
+		public $geometry;
+		public $properties;
+
+		public function __construct($data = null)
+		{
+			parent::__construct($data);
+
+			$this->properties = new \stdClass();
+
+			$this->type = $this->className(false);
+		}
+
+		public function toJson($type = "full")
+		{
+			$properties = $this->getProperties();
+
+			foreach ($properties as $property)
+			{
+				$value = $this->{$property->name};
+
+				if ($value !== null)
+				{
+					$subType = ($property->name == "coordinates") ? "simple" : "full";
+
+					if (is_array($value))
+					{
+						foreach ($value as &$element)
+						{
+							if (is_object($element) && get_class($element) != "stdClass")
+							{
+								$element = json_decode($element->toJson($subType));
+							}
+						}
+
+						if ($value === array())
+						{
+							$value = null;
+						}
+					}
+
+					if (is_object($value))
+					{
+						if (get_class($value) != "stdClass")
+						{
+							$value = json_decode($value->toJson($subType));
+						}
+					}
+
+					if ($value !== null)
+					{
+						$results[$property->name] = $value;
+					}
+				}
+			}
+
+			if ($type == "simple")
+			{
+				$results = $results['coordinates'];
+			}
+
+			return json_encode($results);
+		}
+	}
+?>
