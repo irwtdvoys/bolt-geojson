16869a0b15e6bd5b1fac12101de34407559c0343
Steve Adams <steve@3sidedcube.com>
2015-09-08 16:18:07 +0100

    Add abstract Geometry Class based on old code

---
+++ src/Geometry.php
@@ -0,0 +1,89 @@
+<?php
+	namespace Bolt\GeoJson;
+
+	use \Bolt\Base;
+
+	abstract class Geometry extends Base
+	{
+		public $type;
+		public $coordinates;
+
+		public function __construct($data = null)
+		{
+			parent::__construct($data);
+			$this->type = $this->className(false);
+		}
+
+		public function toJson($type = "full")
+		{
+			$properties = $this->getProperties();
+
+			foreach ($properties as $property)
+			{
+				$value = $this->{$property->name};
+
+				if ($value !== null)
+				{
+					$subType = ($property->name == "coordinates") ? "simple" : "full";
+
+					if (is_array($value))
+					{
+						foreach ($value as &$element)
+						{
+							if (is_object($element) && get_class($element) != "stdClass")
+							{
+								$element = json_decode($element->toJson($subType));
+							}
+						}
+
+						if ($value === array())
+						{
+							$value = null;
+						}
+					}
+
+					if (is_object($value))
+					{
+						if (get_class($value) != "stdClass")
+						{
+							$value = json_decode($value->toJson($subType));
+						}
+					}
+
+					if ($value !== null)
+					{
+						$results[$property->name] = $value;
+					}
+				}
+			}
+
+			if ($type == "simple")
+			{
+				$results = $results['coordinates'];
+			}
+
+			return json_encode($results);
+		}
+
+		public function toEnvelope()
+		{
+			$envelope = new Geometry\Envelope();
+
+			foreach ($this->points() as $point)
+			{
+				$envelope->extend($point);
+			}
+
+			return $envelope;
+		}
+
+		public function toPoint()
+		{
+			$envelope = $this->toEnvelope();
+
+			return $envelope->toPoint();
+		}
+
+		abstract public function points();
+	}
+?>
