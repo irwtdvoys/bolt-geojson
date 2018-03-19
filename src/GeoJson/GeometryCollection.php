4991e591da5f01c427991cfbb29a0bcbffa3256f
Steve Adams <irwtdvoys@gmail.com>
2018-03-19 17:23:16 +0000

    Add missing GeometryCollection model

---
+++ src/GeoJson/GeometryCollection.php
@@ -0,0 +1,79 @@
+<?php
+	namespace Bolt\GeoJson;
+
+	use Bolt\Base;
+
+	class GeometryCollection extends Base
+	{
+		public $type;
+		public $geometries = array();
+
+		public function __construct($data = null)
+		{
+			parent::__construct($data);
+
+            if (!empty($this->geometries))
+            {
+            	foreach ($this->geometries as &$geometry)
+				{
+					$className = "\\Bolt\\GeoJson\\Geometry\\" . $geometry->type;
+
+					$geometry = new $className($geometry);
+				}
+            }
+
+			$this->type = $this->className(false);
+		}
+
+		public function toJson($type = "full")
+		{
+			$properties = $this->getProperties();
+
+			foreach ($properties as $property)
+			{
+				$value = $this->{$property->name};
+
+				if ($value !== null)
+				{
+					$subType = ($property->name == "coordinates") ? "simple" : "full";
+
+					if (is_array($value))
+					{
+						foreach ($value as &$element)
+						{
+							if (is_object($element) && get_class($element) != "stdClass")
+							{
+								$element = json_decode($element->toJson($subType));
+							}
+						}
+
+						if ($value === array())
+						{
+							$value = null;
+						}
+					}
+
+					if (is_object($value))
+					{
+						if (get_class($value) != "stdClass")
+						{
+							$value = json_decode($value->toJson($subType));
+						}
+					}
+
+					if ($value !== null)
+					{
+						$results[$property->name] = $value;
+					}
+				}
+			}
+
+			if ($type == "simple")
+			{
+				$results = $results['coordinates'];
+			}
+
+			return json_encode($results);
+		}
+	}
+?>
